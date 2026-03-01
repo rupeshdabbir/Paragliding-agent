14284a324cb8429f7388e4d553d4980cfed0079c
Rupesh Dabbir <d.rupeshkumar@gmail.com>
2026-02-28 22:25:36 -0800

    feat: Set MapView as the default route and remove the dedicated /map path.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
-import Landing from './pages/Landing.jsx';
 import Chat from './pages/Chat.jsx';
 import MapView from './pages/MapView.jsx';
 
@@ -9,9 +8,8 @@ export default function App() {
         <>
             <Navbar />
             <Routes>
-                <Route path="/" element={<Landing />} />
+                <Route path="/" element={<MapView />} />
                 <Route path="/chat" element={<Chat />} />
-                <Route path="/map" element={<MapView />} />
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </>
