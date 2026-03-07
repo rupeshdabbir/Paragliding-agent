62dade1113e3ecaf7a557cb79020a1fce7dd4457
Rupesh Dabbir <d.rupeshkumar@gmail.com>
2026-03-07 14:12:13 -0800

    feat: Configure Vercel rewrites to serve client-side PWA assets and update the service worker's precache for `index.html`.

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ctl8q7heob8"
+    "revision": "0.nsmddv7aioo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
